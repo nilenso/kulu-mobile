ebbf201a52835e142132810375e72be2681a1999
Akshay Gupta <kitallis@gmail.com>
2014-11-07 18:18:34 +0530

    removing unused methods

--- app/src/main/java/nilenso/com/kulu_mobile/FileUtils.java
+++ app/src/main/java/nilenso/com/kulu_mobile/FileUtils.java
@@ -1,28 +1,10 @@
 package nilenso.com.kulu_mobile;
 
 import org.apache.commons.io.FilenameUtils;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import java.io.File;
 
 public class FileUtils {
-    public static String getPrettyPrintedDate(File file) {
-        DateTime dt = new DateTime(file.lastModified());
-        DateTimeFormatter fmt = DateTimeFormat.forPattern("MMMM dd, yyyy, hh:mm a");
-        return fmt.print(dt);
-    }
-
-    public static String getBaseNameForFile(File file) {
-        String name = file.getName();
-        int pos = name.lastIndexOf(".");
-        if (pos > 0) {
-            name = name.substring(0, pos);
-        }
-        return name;
-    }
-
     public static String getLastPartOfFile(String file) {
         return FilenameUtils.getBaseName(file) + "." + FilenameUtils.getExtension(file);
     }
