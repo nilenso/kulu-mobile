67b7f2e4182b91f8c35213d6f1ca60fdb1e16c66
Smit Shah <who828@gmail.com>
2014-12-03 18:10:54 +0530

    Send expenseDate to kulu backend

--- app/src/main/java/nilenso/com/kulu_mobile/KuluBackend.java
+++ app/src/main/java/nilenso/com/kulu_mobile/KuluBackend.java
@@ -6,9 +6,11 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
+import org.joda.time.DateTime;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,13 +29,14 @@ public class KuluBackend {
         String remarksKey = "remarks";
         String expenseTypeKey = "expense_type";
         String dateKey = "date";
+        Date expenseDate = expense.getCreatedAt();
 
         Map<String, String> requestMap = new HashMap<String, String>();
 
         requestMap.put(requestKey, FileUtils.getLastPartOfFile(s3Location));
         requestMap.put(remarksKey, expense.getComments());
         requestMap.put(expenseTypeKey, expense.getExpenseType());
-        requestMap.put(dateKey , expense.getCreatedAt().toString());
+        requestMap.put(dateKey , new DateTime(expenseDate).toString("yyyy-MM-dd"));
 
         JSONObject json = new JSONObject(requestMap);
 
